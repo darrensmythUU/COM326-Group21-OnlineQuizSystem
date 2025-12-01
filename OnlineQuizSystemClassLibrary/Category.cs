b7599f19595f540c1589868eb49613413cfaf13d
Kiphea <djsmyth05@gmail.com>
2025-12-01 14:51:16 +0000

    Added Category class fields, get/sets, constructors and a method to display info

--- OnlineQuizSystemClassLibrary/Category.cs
+++ OnlineQuizSystemClassLibrary/Category.cs
@@ -1,13 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace OnlineQuizSystemClassLibrary
 {
-    internal class Category
+    public class Category
     {
+        // Private Fields
+        private int id;
+        private string name;
+        private string description;
+        private static int count = 0;
 
+        // Get/Set Constructors
+        public int ID
+        {
+            get { return id; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        // Default Constructor for Category object
+        public Category()
+        {
+            this.id = count;
+            this.Name = "";
+            this.Description = "";
+            count++;
+        }
+
+        // Paramaterised Constructor for Category object
+        public Category(string categoryName, string categoryDescription)
+        {
+            this.id = count;
+            this.Name = categoryName;
+            this.Description = categoryDescription;
+            count++;
+        }
+
+        // Methods
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Category ID: {ID}");
+            Console.WriteLine($"Category Name: {Name}");
+            Console.WriteLine($"Category Description: {Description}");
+            Console.WriteLine();
+        }
     }
 }
