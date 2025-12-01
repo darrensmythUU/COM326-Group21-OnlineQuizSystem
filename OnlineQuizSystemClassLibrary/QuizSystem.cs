69daf05c78136e6889f102c4818a971be640532f
Kiphea <djsmyth05@gmail.com>
2025-12-01 15:22:06 +0000

    Added QuizSystem fields, get/sets and constructors

--- OnlineQuizSystemClassLibrary/QuizSystem.cs
+++ OnlineQuizSystemClassLibrary/QuizSystem.cs
@@ -1,13 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace OnlineQuizSystemClassLibrary
 {
-    internal class QuizSystem
+    public class QuizSystem
     {
+        // Private Fields
+        private List<User> users;
+        private List<Quiz> quizzes;
+        private List<Category> categories;
+        private List<Question> questionBank;
 
+        // Public Get/Set Constructors
+        public List<User> Users
+        {
+            get { return users; }
+            set { users = value; }
+        }
+
+        public List<Quiz> Quizzes
+        {
+            get { return quizzes; }
+            set { quizzes = value; }
+        }
+
+        public List<Category> Categories
+        {
+            get { return categories; }
+            set { categories = value; }
+        }
+
+        public List<Question> QuestionBank
+        {
+            get { return questionBank; }
+            set { questionBank = value; }
+        }
+
+        // Default Constructor for QuizSystem object
+        public QuizSystem()
+        {
+            this.Users = new List<User>();
+            this.Quizzes = new List<Quiz>();
+            this.Categories = new List<Category>();
+            this.QuestionBank = new List<Question>();
+        }
+
+        // Paramaterised Constructor for QuizSystem object
+        public QuizSystem(List<Question> users, List<Question> quizzes, List<Question> categories, List<Question> questionBank)
+        {
+            this.Users = users;
+            this.Quizzes = quizzes;
+            this.Categories = categories;
+            this.QuestionBank = questionBank;
+        }
+
+        // Methods
     }
 }
