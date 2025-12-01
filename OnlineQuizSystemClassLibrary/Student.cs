20d479c5d2231392c7ec26d9c56c4129aef40b00
Kiphea <djsmyth05@gmail.com>
2025-12-01 14:42:07 +0000

    Added Student class fields, get/sets, constructors and a method to display info

--- OnlineQuizSystemClassLibrary/Student.cs
+++ OnlineQuizSystemClassLibrary/Student.cs
@@ -1,13 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace OnlineQuizSystemClassLibrary
 {
-    internal class Student
+    public class Student : User
     {
+        // Private Fields
+        private string status;
 
+        // Get/Set Constructors
+        public string Status
+        {
+            get { return status; }
+            set { status = value; }
+        }
+
+        // Default Constructor for Student object
+        public Student()
+        {
+            this.id = count;
+            this.Username = "";
+            this.Password = "";
+            this.Email = "";
+            this.Role = "";
+            this.Status = "Inactive";
+            count++;
+        }
+
+        // Paramaterised Constructor for Student object
+        public Student(string userUsername, string userPassword, string userEmail, string userRole, string userStatus)
+        {
+            this.id = count;
+            this.Username = userUsername;
+            this.Password = userPassword;
+            this.Email = userEmail;
+            this.Role = userRole;
+            this.Status = userStatus;
+            count++;
+        }
+
+        // Methods
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Student ID: {ID}");
+            Console.WriteLine($"Student Userame: {Username}");
+            Console.WriteLine($"Student Password: {Password}");
+            Console.WriteLine($"Student Email: {Email}");
+            Console.WriteLine($"Student Role: {Role}");
+            Console.WriteLine($"Student Status: {Status}");
+            Console.WriteLine();
+        }
     }
 }
