21ff94a3fc852c2de83b1d353e3565073578f4f9
Kiphea <djsmyth05@gmail.com>
2025-12-01 14:39:36 +0000

    Added Admin class fields, constructors, and method to display info

--- OnlineQuizSystemClassLibrary/Admin.cs
+++ OnlineQuizSystemClassLibrary/Admin.cs
@@ -7,8 +7,51 @@ using System.Xml.Linq;
 
 namespace OnlineQuizSystemClassLibrary
 {
-    internal class Admin
+    public class Admin : User
     {
+        // Private Fields
+        private DateTime loginDate;
 
+        // Get/Set Constructors
+        public DateTime LoginDate
+        {
+            get { return loginDate; }
+            set { loginDate = value; }
+        }
+
+        // Default Constructor for Admin object
+        public Admin()
+        {
+            this.id = count;
+            this.Username = "";
+            this.Password = "";
+            this.Email = "";
+            this.Role = "";
+            count++;
+        }
+
+        // Paramaterised Constructor for Admin object
+        public Admin(string userUsername, string userPassword, string userEmail, string userRole, DateTime AdminLoginDate)
+        {
+            this.id = count;
+            this.Username = userUsername;
+            this.Password = userPassword;
+            this.Email = userEmail;
+            this.Role = userRole;
+            this.LoginDate = DateTime.Now;
+            count++;
+        }
+
+        // Methods
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Admin ID: {ID}");
+            Console.WriteLine($"Admin Userame: {Username}");
+            Console.WriteLine($"Admin Password: {Password}");
+            Console.WriteLine($"Admin Email: {Email}");
+            Console.WriteLine($"Admin Role: {Role}");
+            Console.WriteLine($"Admin's Last Login Date: {LoginDate}");
+            Console.WriteLine();
+        }
     }
 }
