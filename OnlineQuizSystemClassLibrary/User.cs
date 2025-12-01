33d69b82c00ab4f5eb31183f9e07efdd47b17f31
Kiphea <djsmyth05@gmail.com>
2025-12-01 14:08:40 +0000

    Added properties, get/sets, and started work on Constructors

--- OnlineQuizSystemClassLibrary/User.cs
+++ OnlineQuizSystemClassLibrary/User.cs
@@ -2,6 +2,52 @@
 {
     public class User
     {
+        // Private Fields
+        private int id;
+        private string username;
+        private string password;
+        private string email;
+        private string role;
 
+        // Get/Set Constructors
+        public int ID
+        {
+            get { return id; }
+        }
+
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        public string Role
+        {
+            get { return role; }
+            set { role = value; }
+        }
+
+        // Constructor for User object
+        public User(string userUsername, string userPassword, string userEmail, string userRole)
+        {
+            this.Username = userUsername;
+            this.Password = userPassword;
+            this.Email = userEmail;
+            this.Email = userRole;
+        }
+
+        // Methods
     }
 }
